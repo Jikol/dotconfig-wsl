cfd694eb63cac93dd568d81a90ba7a6e544cedbe
Ondřej Javorský <gikol113@gmail.com>
2024-03-24 03:20:57 +0100

    -

--- fish/config.fish
+++ fish/config.fish
@@ -39,13 +39,13 @@ set fish_greeting
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f ~/miniconda3/bin/conda
-    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f /home/jikol/miniconda3/bin/conda
+    eval /home/jikol/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "~/miniconda3/etc/fish/conf.d/conda.fish"
-        . "~/miniconda3/etc/fish/conf.d/conda.fish"
+    if test -f "/home/jikol/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/jikol/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "~/miniconda3/bin" $PATH
+        set -x PATH "/home/jikol/miniconda3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
